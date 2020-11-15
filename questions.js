d859dde0166cc94a788cf582e3a0505464f5cce7
19PA1A0509 <60499580+19PA1A0509@users.noreply.github.com>
2020-11-15 18:39:20 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,172 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocesser",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "If David’s age is 27 years old in 2011. What was his age in 2003?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "17 years",
+            "37 years",
+            "20 years",
+            "19 years"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Speed of a car is 60 km/hr. Distance covered in 1 ¼ hours is ………",
+        answer: "75 km.",
+        options: [
+            "60 km.",
+            "65 km.",
+            "70 km.",
+            "75 km."
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Which of the following is the oldest test cricket playing country in the world?",
+        answer: "Australia",
+        options: [
+            "Australia",
+            "India",
+            "West Indies",
+            "South Africa"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which cricketer had scored highest individual score in ODI cricket?",
+        answer: "Rohit Sharma",
+        options: [
+            "Virender Sehwag",
+            "Chris Gayle",
+            "Martin Guptill",
+            "Rohit Sharma"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Who is the first Indian to win a Nobel Prize?",
+        answer: "Rabindranath Tagore",
+        options: [
+            "Rabindranath Tagore",
+            "CV Raman",
+            "Mother Theresa",
+            "Hargobind Khorana"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Choose the correct option..This Pen is much ......... than that.",
+        answer: "cheaper",
+        options: [
+            "cheaper",
+            "more cheaper",
+            "the cheaper",
+            "cheapest"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which among the following is the smallest bird in the world?",
+        answer: "Hummingbird",
+        options: [
+            "Hummingbird",
+            "Weebill",
+            "Goldcrest",
+            "Pardalote"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 12,
+        question: "He told me that he …….. watching the movie.",
+        answer: "had finished",
+        options: [
+            "is finished",
+            "was finished",
+            "had finished",
+            "not finished"
+        ]
+    },
+    {
+        numb: 13,
+        question: "How will our sun end up it's life after around 6 billion years?",
+        answer: "white dwarf",
+        optios: [
+            "Black hole",
+            "neutron star",
+            "white dwarf",
+            "pulsar"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    {
+        numb: 15,
+        question: "How many galaxies can be seen with naked eye from the earth?",
+        answer: "4 galaxies",
+        options: [
+            "25 galaxies",
+            "none",
+            "4 galaxies",
+            "1 galaxy"
+        ]
+    }
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+];
